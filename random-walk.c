63d1aff103d8d14156bab663ee8a45ea1956b01f
batabutter <ipibym@gmail.com>
2026-01-06 23:50:11 -0500

    added different direcitons and fixed time

--- random-walk.c
+++ random-walk.c
@@ -9,11 +9,21 @@
 #define WIDTH 500
 #define HEIGHT 500
 
+#define FRAME_TIME 100.0f
+
+typedef enum {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
+} Direction;
+
 typedef struct {
 
     int x;
     int y;
     Uint32 color;
+    
 
 }Agent;
 
@@ -21,22 +31,42 @@ void draw_agents(Agent *agents, int num_agents, SDL_Surface *surface) {
 
     int dist = (rand() % (20 - 10 + 1) + 10);
     double dir_calc = ((double) rand() / (double) RAND_MAX);
-    int dir = 0;
-   
-    if (dir_calc > .5)
-        dir = 1;
-    else
-        dir = 0;
+    Direction dir = DOWN;
+ 
+    if (dir_calc < .25)
+        dir = UP;
+    else if (dir_calc >= .25 && dir_calc <= .5)
+        dir = DOWN;
+    else if (dir_calc >.5 && dir_calc < .75)
+        dir = LEFT;
+    else {
+        dir = RIGHT;
+    }
+
 
     SDL_Rect rect;
     for (int i = 0; i < num_agents; i++) {
         
-        if (dir) {
-            rect = (SDL_Rect) {agents[i].x, agents[i].y, dist, 5};
-            agents[i].x = agents[i].x + dist;
-        } else {
-            rect = (SDL_Rect) {agents[i].x, agents[i].y, 5, dist};
-            agents[i].y = agents[i].y + dist;
+        switch (dir) {
+            case DOWN:
+                rect = (SDL_Rect) {agents[i].x, agents[i].y, 5, dist};
+                agents[i].y = agents[i].y + dist;
+            break;
+            
+            case LEFT:
+                rect = (SDL_Rect) {agents[i].x - dist, agents[i].y, dist, 5};
+                agents[i].x = agents[i].x - dist;
+            break;
+            
+            case RIGHT:
+                rect = (SDL_Rect) {agents[i].x, agents[i].y, dist, 5};
+                agents[i].x = agents[i].x + dist;
+            break;
+            
+            case UP:
+                rect = (SDL_Rect) {agents[i].x, agents[i].y - dist, 5, dist};
+                agents[i].y = agents[i].y - dist;
+            break; 
         }
         SDL_FillRect (surface, &rect, agents[i].color); 
         SDL_Log("Drawing at %d, %d to (%d, %d))", rect.x, rect.y, agents[i].x, agents[i].y);
@@ -48,7 +78,7 @@ void draw_agents(Agent *agents, int num_agents, SDL_Surface *surface) {
 int main(int argc, const char *argv[]) {
     
     int num_agents;    
-
+    srand(time(NULL));
 
     if (argc == 1) {
         num_agents = DEFAULT_AGENTS;
@@ -74,28 +104,26 @@ int main(int argc, const char *argv[]) {
     Agent agents[1];
     agents[0] = (Agent) {100, 100, 0xFFFFFFFF};
     
-    struct timespec start, end;
-    
-    timespec_get(&start, TIME_UTC);
-    float delta_time;
+    uint32_t start_time;
+    float frame_time;
+
+    start_time = SDL_GetTicks();
     while (app_running) {
         
-        timespec_get(&end, TIME_UTC);
-        delta_time = (end.tv_nsec - start.tv_nsec) / 1000000.0f;
+        frame_time = SDL_GetTicks() - start_time;
+
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) {
                 app_running = 0;
             }
         }
        
-
-
-       SDL_Log("Delta (ms) : %f", delta_time);
-        if (delta_time >= 16.66f) {
+        //SDL_Log("Delta (ms) : %f", frame_time);
+        if (frame_time >= FRAME_TIME) {
             SDL_UpdateWindowSurface(pwindow);
-
+            //SDL_Log("Updating the window");
             draw_agents(agents, 1, psurface);
-            timespec_get(&start, TIME_UTC);
+            start_time = SDL_GetTicks();
         }
     }
 
